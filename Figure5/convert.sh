76df1ef34e7fe30781cd39f0a6a7637d07a03639
Hitoshi Iuchi <hitoshi.iuchi@gmail.com>
2020-08-27 20:57:05 +0900

    Update convert.sh

--- Figure5/convert.sh
+++ Figure5/convert.sh
@@ -4,6 +4,6 @@ wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.ensGene.g
 gunzip hg19.ensGene.gtf.gz
 nohup find . -name './files/*.Human.*.bam' | parallel -j 64 'htseq-count -s reverse -f bam {} ./hg19.ensGene.gtf > ./results/{.}.txt'
 
-wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/genes/mm10.ensGene.gtf.gz
-gunzip mm10.ensGene.gtf.gz
-nohup find . -name './files/*.Mouse.*.bam' | parallel -j 64 'htseq-count -s reverse -f bam {} ./mm10.ensGene.gtf > ./results/{.}.txt'
+wget ftp://ftp.ensembl.org/pub/release-101/gtf/mus_musculus/Mus_musculus.GRCm38.101.gtf.gz
+gunzip Mus_musculus.GRCm38.101.gtf.gz
+nohup find . -name './files/*.Mouse.*.bam' | parallel -j 64 'htseq-count -s reverse -f bam {} ./Mus_musculus.GRCm38.101.gtf > ./results/{.}.txt'
