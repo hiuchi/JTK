b9b62b4a16f0e3bb52df2a6a5790acd67626a62f
Hitoshi Iuchi <hitoshi.iuchi@gmail.com>
2021-02-15 14:31:08 +0900

    Delete Figure5 directory

--- Figure5/convert.sh
+++
@@ -1,9 +0,0 @@
-#This is the source code to reproduce Figure 5.
-
-wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.ensGene.gtf.gz
-gunzip hg19.ensGene.gtf.gz
-nohup find . -name './files/*.Human.*.bam' | parallel -j 64 'htseq-count -s reverse -f bam {} ./hg19.ensGene.gtf > ./results/{.}.txt'
-
-wget ftp://ftp.ensembl.org/pub/release-101/gtf/mus_musculus/Mus_musculus.GRCm38.101.gtf.gz
-gunzip Mus_musculus.GRCm38.101.gtf.gz
-nohup find . -name './files/*.Mouse.*.bam' | parallel -j 64 'htseq-count -s reverse -f bam {} ./Mus_musculus.GRCm38.101.gtf > ./results/{.}.txt'
